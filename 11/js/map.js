d7ed358b0f4a7c21cf5724d21b6fb2511bd3e528
Keks <keks@htmlacademy.ru>
2018-10-23 03:56:54 +0300

    :heavy_check_mark: Сборка #11

--- 11/js/map.js
+++ 11/js/map.js
@@ -1,24 +1,16 @@
-ymaps.ready(function () {
+function initMap() {
   "use strict";
-    var myMap = new ymaps.Map('map', {
-    center: [59.9387515, 30.323040],
+  var coordinates = {lat: 59.9387515, lng: 30.323040};
+  var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 16,
-    controls: []
-  }, {
-    searchControlProvider: 'yandex#search'
-  }),
-
-  myPlacemark = new ymaps.Placemark([59.9387515, 30.323040],
-
-  {balloonContentBody: "<div style='font-family: @font-family;'><div style='font-weight: bold;'>Магазин Mishka</div><div>г. Санкт-Петербург, ул. Большая Конюшенная,<br> д. 19/8, офис 101</div></div>"},
-  {
-    iconLayout: 'default#image',
-    iconImageHref: 'img/icon-map-pin.svg',
-    iconImageSize: [66, 101],
-    iconImageOffset: [-25, -80]
+    center: coordinates,
+    disableDefaultUI: true,
+    scrollwheel: false
   });
 
-  myMap.geoObjects.add(myPlacemark);
-  myMap.behaviors.disable(['scrollZoom', 'drag']);
-
-});
+  var marker = new google.maps.Marker({
+  position: coordinates,
+  map: map,
+  icon: 'img/icon-map-pin.svg'
+  });
+}
